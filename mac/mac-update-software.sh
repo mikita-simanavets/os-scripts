ed5c691481087f8f5aa110ac72c7ac5763549eb3
Mikita Simanavets <sema.nikita@gmail.com>
2018-03-22 22:36:12 +0100

    Update mac update scipt with mas, brew, apm and npm

--- mac/mac-update-software.sh
+++ mac/mac-update-software.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-echo "(1/5) Updating by System Software Update tool"
-sudo softwareupdate --install --recommended --verbose
+echo "(1/7) Updating Mac App Store applications"
+mas outdated
+read -p "Do you want to run mas upgrade? (y/N) " choice
+case "$choice" in
+  y|Y ) mas upgrade;;
+  n|N|* ) echo "mas update skipped";;
+esac
 
-echo "(2/5) Updating Homebrew packages"
+echo "(2/7) Updating Homebrew packages"
 brew update
-brew upgrade --cleanup --verbose
+brew upgrade --cleanup
 
-echo "(3/5) Updating Homebrew-Cask packages"
-brew cask upgrade --verbose
-brew cask upgrade --greedy --verbose
-brew cask cleanup --verbose
+echo "(3/7) Updating Homebrew-Cask packages"
+brew cask outdated | cut -f 1 | xargs brew cask reinstall
+brew cask cleanup
 
-echo "(4/5) Checking system for potential problems with Homebrew packages"
+echo "(4/7) Updating Atom text editor plugins"
+apm update --no-confirm
+
+echo "(5/7) Updating all the npm global installed binaries"
+npm update --global
+
+echo "(6/7) Checking system for potential problems with Homebrew packages"
 brew doctor
 brew missing
 brew prune
 
-echo "(5/5) Checking system for potential problems with Homebrew-Cask packages"
+echo "(7/7) Checking system for potential problems with Homebrew-Cask packages"
 brew cask doctor
